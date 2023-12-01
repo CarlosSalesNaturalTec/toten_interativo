2862782944dfb1a8b17b8db511edb1e2d47f48b8
Carlos Sales <causalvador@hotmail.com>
2023-12-01 11:25:56 -0300

    Pitch mais rápido fala

--- public/src/config.js
+++ public/src/config.js
@@ -2,7 +2,7 @@
 
 const messages = {
     "pitch": 1,     // Pitch = Tom da fala, mais grave ou mais agudo. Valor Default
-    "rate": 1,    // Rate = Velocidade da fala. Valor Default
+    "rate": 1.3,    // Rate = Velocidade da fala. Valor Default
 
     //"input_name": "  Olá. Seja bem vindo. Por favor informe o seu nome.",
     "input_name": " Oi, me pergunte algo.",
