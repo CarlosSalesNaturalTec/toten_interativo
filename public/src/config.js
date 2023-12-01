25d0155e455311e8fc277fd0cca16b70980ae62c
Carlos Sales <causalvador@hotmail.com>
2023-12-01 11:17:13 -0300

    entonação de voz

--- public/src/config.js
+++ public/src/config.js
@@ -1,8 +1,8 @@
 // Mensagens exibidas no aplicativo. Trocar textos em caso de internacionalização
 
 const messages = {
-    "pitch": 2,     // Pitch = Tom da fala, mais grave ou mais agudo. Valor Default
-    "rate": 2,    // Rate = Velocidade da fala. Valor Default
+    "pitch": 1,     // Pitch = Tom da fala, mais grave ou mais agudo. Valor Default
+    "rate": 1,    // Rate = Velocidade da fala. Valor Default
 
     //"input_name": "  Olá. Seja bem vindo. Por favor informe o seu nome.",
     "input_name": " Oi, me pergunte algo.",
